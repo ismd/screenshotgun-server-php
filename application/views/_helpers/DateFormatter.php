8899051c1e6fc1b2b60825edf6dadc33b8bacc83
Vladimir Kosteley <zzismd@gmail.com>
2015-11-07 18:41:32 +0600

    Date format fix

--- application/views/_helpers/DateFormatter.php
+++ application/views/_helpers/DateFormatter.php
@@ -5,6 +5,6 @@ class DateFormatterViewHelper extends PsViewHelper {
     private $months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 
     public function showPageFormat(DateTime $dateTime) {
-        return $dateTime->format('d ' . $this->months[$dateTime->format('n') - 1] . ' Y');
+        return $dateTime->format('j ' . $this->months[$dateTime->format('n') - 1] . ' Y');
     }
 }
